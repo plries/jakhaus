22014ca477ea00ececefade1a6c4630192a96f52
plries <apaulries@gmail.com>
2025-06-16 17:29:57 -0700

    Navbar imports

--- app/(site)/[id]/components/Listing/index.tsx
+++ app/(site)/[id]/components/Listing/index.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from "next";
 import { IconContext } from "@phosphor-icons/react";
 import {
-  NavBar,
+  Navbar,
   Overview,
   Photos,
   Video,
@@ -21,7 +21,7 @@ export const Listing = ({ CONSTANTS, FULL_ADDRESS }: SectionPropTypes) => {
 
   return (
     <IconContext.Provider value={{ size: 24, weight: "light" }}>
-      <NavBar CONSTANTS={CONSTANTS} />
+      <Navbar CONSTANTS={CONSTANTS} />
       <Overview CONSTANTS={CONSTANTS} FULL_ADDRESS={FULL_ADDRESS} />
       <div className="grid grid-cols-4 gap-5 md:grid-cols-8 lg:grid-cols-12">
         <div className="col-span-full grid grid-cols-4 gap-5 border-x border-x-neutral-300 pt-5 pb-10 md:grid-cols-8 lg:col-span-10 lg:col-start-2 lg:grid-cols-10">

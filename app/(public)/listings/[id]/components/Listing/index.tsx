7993746ca2b6b9dae19f8d6376ae7ae6fdf9d320
plries <apaulries@gmail.com>
2025-06-21 00:49:08 -0700

    updated listing const

--- app/(public)/listings/[id]/components/Listing/index.tsx
+++ app/(public)/listings/[id]/components/Listing/index.tsx
@@ -24,13 +24,13 @@ export const Listing = ({ CONSTANTS, FULL_ADDRESS }: SectionPropTypes) => {
       <div className="grid grid-cols-4 gap-5 md:grid-cols-8 lg:grid-cols-12">
         <div className="col-span-full grid grid-cols-4 gap-5 py-5 md:grid-cols-8 lg:col-span-10 lg:col-start-2 lg:mx-2.5 lg:grid-cols-10 lg:border-x lg:border-x-neutral-300 lg:py-10">
           <Photos CONSTANTS={CONSTANTS} FULL_ADDRESS={FULL_ADDRESS} />
-          {CONSTANTS.VIDEO_LINK && (
+          {CONSTANTS.OTHER_ATTACHMENTS?.VIDEO_LINK && (
             <Video CONSTANTS={CONSTANTS} FULL_ADDRESS={FULL_ADDRESS} />
           )}
-          {CONSTANTS.FLOOR_PLAN && (
+          {CONSTANTS.OTHER_ATTACHMENTS?.FLOOR_PLAN && (
             <FloorPlan CONSTANTS={CONSTANTS} FULL_ADDRESS={FULL_ADDRESS} />
           )}
-          {CONSTANTS.SCAN_LINK && (
+          {CONSTANTS.OTHER_ATTACHMENTS?.SCAN_LINK && (
             <VirtualTour CONSTANTS={CONSTANTS} FULL_ADDRESS={FULL_ADDRESS} />
           )}
         </div>

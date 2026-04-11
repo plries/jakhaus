97b17167557f15e88e4e7ab6ebd23be0d6b5374d
Paul Aries <apaulries@gmail.com>
2026-04-11 16:14:52 -0700

    add analytics

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,4 +1,5 @@
 import ReactLenis from "lenis/react";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function PublicLayout({
   children,
@@ -7,6 +8,7 @@ export default function PublicLayout({
 }>) {
   return (
     <>
+      <Analytics />
       <ReactLenis root options={{ lerp: 0.05 }}>
         <main>{children}</main>
       </ReactLenis>
